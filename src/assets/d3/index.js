9fc3e63dd23b1efc20244ac3d2effbb2db2feaa7
ErwinFeld <erwinmailkonto@gmail.com>
2021-09-17 14:56:10 +0200

    added d3 imports

---
+++ src/assets/d3/index.js
@@ -0,0 +1,23 @@
+import { select, selectAll } from 'd3-selection';
+
+import { hierarchy, tree } from 'd3-hierarchy';
+
+import { linkHorizontal } from 'd3-shape';
+
+
+import { color} from 'd3-color'
+
+export default {
+    select,
+    selectAll,
+
+    hierarchy,
+    tree,
+
+    linkHorizontal,
+
+    color,
+   
+  };
+
+  
