537a883cd0eb690113d8a23d47fca6ede2a8d1e5
ErwinFeld <erwinmailkonto@gmail.com>
2021-09-28 12:50:53 +0200

    added installed packages to imports

--- src/assets/d3/index.js
+++ src/assets/d3/index.js
@@ -1,9 +1,13 @@
+
 import { select, selectAll, create } from 'd3-selection';
 
 import { hierarchy, tree } from 'd3-hierarchy';
 
 import { linkHorizontal } from 'd3-shape';
 
+import { zoom } from 'd3-zoom'
+
+import { transition } from 'd3-transition';
 
 import { color} from 'd3-color'
 
@@ -16,6 +20,10 @@ export default {
     tree,
 
     linkHorizontal,
+
+    zoom,
+
+    transition,
 
     color,
    
