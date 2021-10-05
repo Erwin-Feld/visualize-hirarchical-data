3bca69f83e2ad30219779451d504db9f3dd1b5d8
ErwinFeld <erwinmailkonto@gmail.com>
2021-10-05 18:30:20 +0200

    added popUp vue component to display error message

---
+++ src/components/PopUp.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{errorMessage}}</h1>
+     <button @click="changeData">x</button>
+</template>
+
+<script>
+export default {
+    name:"PopUp",
+    props: ["errorMessage", "dissmiss"],
+    emits: ["change-value"],
+
+
+    methods: {
+        changeData(){
+            this.$emit("change-value", false)
+        }
+    }
+    
+}
+</script>
