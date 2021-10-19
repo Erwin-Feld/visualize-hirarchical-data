6af0188de66351530f33e416d9017645c618427b
ErwinFeld <erwinmailkonto@gmail.com>
2021-10-19 20:39:50 +0200

    add to component to hide slider if cookie was excepeted

--- src/components/CookieSlider.vue
+++ src/components/CookieSlider.vue
@@ -1,19 +1,52 @@
 <template>
-  <div class="cookie-slider">
+  <div class="cookie-slider-true" :class="{cookiefalse :allowCookies}">
     <div class="consent-container">
       <p>This website uses cookies. For more information read our</p>
       <router-link to="/policy"> 🍪 policy</router-link>
-      <button class="consent-button" type="button">Got it!</button>
+      <button 
+        class="consent-button" 
+        type="button"
+        @click="okBannerClicked"
+       >Got it!</button>
     </div>
   </div>
 </template>
 
+<script>
+import { defineComponent,inject, ref } from 'vue';
+import mediumCookies from '../assets/cookieManager/useCookies';
+
+
+const currentState = ref(false);
+
+
+export default defineComponent ({
+  name: 'CookieSlider',
+  setup() {
+    const gtag = inject('gtag');
+
+    // Add brauch ich das ? 
+    const { showBanner, okClicked, allowCookies } = mediumCookies(gtag);
+    // geht net ganz
+    const okBannerClicked = () => okClicked();
+
+
+    return {
+      showBanner,
+      okBannerClicked,
+      allowCookies,   // need to return it to work it !!
+      currentState
+    };
+  },
+});
+</script>
+
 <style scoped>
 
-.cookie-slider {
+.cookie-slider-true {
 
   /* visibility animation after 6 seconds */
-  animation: showBlock 0s 6s forwards;
+  animation: showBlock 0s 1s forwards;
   visibility: hidden;
 
   display: flex;
@@ -61,4 +94,10 @@
 .consent-button:hover {
   font-weight: 600;
 }
+
+
+.cookiefalse {
+  display: none;
+}
+
 </style>
