0f10b9e36e5386414805059a9557a85a1a00c488
ErwinFeld <erwinmailkonto@gmail.com>
2021-10-22 11:26:04 +0200

    added a button added function to button to resset position of graph to default
    added media query to button size

--- src/components/ZoomTreeGraph.vue
+++ src/components/ZoomTreeGraph.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="zoom-tree-graph" ref="divRef"></div>
+  <div class="zoom-tree-graph" ref="divRef">
+    <button type="button" @click="defaultGraphPosition">restore Graph</button>
+  </div>
 </template>
 
 <script>
@@ -61,7 +63,31 @@ export default {
       }
     );
 
-    return { divRef };
+
+    const defaultGraphPosition = () => {
+        d3.selectAll(".svg-graph").remove();
+
+        // *** creates new one
+        // *** variables **********************
+
+        const selectDivRef = d3.select(divRef.value);
+
+        const root = customHierarchy(d3, props.data, makeDataHirarchic);
+
+        //  link length
+        const dx = 60;
+
+        // link height
+        const dy = 80;
+
+        // selects the div parent and append the svg element to it
+        d3.select(selectDivRef.node()).append(function() {
+          return zoomGraph(d3, root, dx, dy, divRef);
+        });
+
+    }
+
+    return { divRef, defaultGraphPosition };
   },
 };
 </script>
@@ -71,6 +97,39 @@ export default {
 .zoom-tree-graph {
   height: 100%;
   width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+}
+
+button {
+  max-width: 80px;
+   font-family: "Roboto Mono", monospace;
+  /* color: #999999; */
+  /* border-color: #F8485E;  */
+
+   border-color: #e2d3f5;
+    /* border-color: #F8B3BB; */
+  border-radius: 11%;
+
+  font-size: 0.5rem;
+}
+
+button:hover {
+   border-color: #D2B6F5;
+  font-weight: 600;
+    /* text-shadow: 1px 2px 2.5px  #F8485E; */
+    /* border-radius: 18%; */
+}
+
+
+
+@media only screen and (max-width: 768px){
+
+button {
+   font-size: 0.8rem;
+}
 
 }
 
