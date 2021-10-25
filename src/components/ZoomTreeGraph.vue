8d7334c77fa04117c5a603cc8afc7c15bf74a44f
ErwinFeld <erwinmailkonto@gmail.com>
2021-10-25 09:48:12 +0200

    aligned restore Graph button with graph added defaultGraphPosition func with changes

--- src/components/ZoomTreeGraph.vue
+++ src/components/ZoomTreeGraph.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="zoom-tree-graph" ref="divRef">
-    <button type="button" @click="defaultGraphPosition">restore Graph</button>
+    <div class="button-container-left">
+      <button type="button" @click="defaultGraphPosition">restore Graph</button>
+    </div>
   </div>
 </template>
 
@@ -52,23 +54,21 @@ export default {
       }
     );
 
-
     const defaultGraphPosition = () => {
-        d3.selectAll(".svg-graph").remove();
+      d3.selectAll(".svg-graph").remove();
 
-        // *** creates new one
-        // *** variables **********************
+      // *** creates new one
+      // *** variables **********************
 
-        const selectDivRef = d3.select(divRef.value);
+      const selectDivRef = d3.select(divRef.value);
 
-        const root = customHierarchy(d3, props.data, makeDataHirarchic);
-       
-        // selects the div parent and append the svg element to it
-        d3.select(selectDivRef.node()).append(function() {
-          return zoomGraph(d3, root, divRef, window.innerWidth);
-        });
+      const root = customHierarchy(d3, props.data, makeDataHirarchic);
 
-    }
+      // selects the div parent and append the svg element to it
+      d3.select(selectDivRef.node()).append(function() {
+        return zoomGraph(d3, root, divRef, window.innerWidth);
+      });
+    };
 
     return { divRef, defaultGraphPosition };
   },
@@ -76,72 +76,66 @@ export default {
 </script>
 
 <style scoped>
-
 .zoom-tree-graph {
   height: 100%;
   width: 100%;
 
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+}
 
+.button-container-left {
+  display: flex;
+  align-items: center;
+  margin-bottom: 200px;
 }
 
 button {
   max-width: 80px;
-   font-family: "Roboto Mono", monospace;
+  font-family: "Roboto Mono", monospace;
   /* color: #999999; */
   /* border-color: #F8485E;  */
 
-   border-color: #e2d3f5;
-    /* border-color: #F8B3BB; */
+  border-color: #e2d3f5;
+  /* border-color: #F8B3BB; */
   border-radius: 11%;
 
   font-size: 0.5rem;
 }
 
 button:hover {
-   border-color: #D2B6F5;
+  border-color: #d2b6f5;
   font-weight: 600;
-    /* text-shadow: 1px 2px 2.5px  #F8485E; */
-    /* border-radius: 18%; */
+  /* text-shadow: 1px 2px 2.5px  #F8485E; */
+  /* border-radius: 18%; */
 }
 
 /* mobile same */
 
 /* Tablet same */
-@media only screen and (max-width: 768px){
-
-button {
-   font-size: 0.8rem;
-}
-
+@media only screen and (min-width: 200px) {
+  button {
+    font-size: 0.8rem;
+  }
 }
 
 /* Laptop 1024 */
-@media only screen and (min-width: 1024px){
-
-button {
-   font-size: 0.8rem;
+@media only screen and (min-width: 1024px) {
+  button {
+    font-size: 0.8rem;
+    max-width: 110px;
+  }
 }
-
-}
-
 
 /* Desktop 1920 */
-@media only screen and (min-width: 1920px){
+@media only screen and (min-width: 1920px) {
+  button {
+    max-width: 110px;
 
-button {
-  max-width: 110px;
-  
-  
     /* border-color: #F8B3BB; */
-  border-radius: 11%;
-  border-width: 3px;
-  font-size: 1rem;
+    border-radius: 11%;
+    border-width: 3px;
+    font-size: 1rem;
+  }
 }
-
-
-}
-
-
 </style>
